ede46a00e2e6f58e781f7002818cc925e8691fa6
Richard Yang <churd@Richards-MacBook-Pro.local>
2022-05-02 12:50:09 -0700

    added decomposible function from yanju and implemented first version of zhash-set! for symbolic terms

--- rhash.rkt
+++ rhash.rkt
@@ -25,18 +25,55 @@
 )
 
 ; cap - capacity, iv - initial value, ev - empty value, k2i - key to index map, vvec - value vector
-(struct zhash (cap iv ev k2i vvec) #:mutable #:transparent #:reflection-name 'zhash)
+(struct zhash (cap iv ev k2i vvec symconstvec) #:mutable #:transparent #:reflection-name 'zhash)
 ; by default, k2i is an empty hash, vvec is a vector of default value ev
-(define (make-zhash cap #:iv [iv zvoid] #:ev [ev zvoid]) (zhash cap iv ev (make-hash) (make-vector cap iv)))
+(define (make-zhash cap #:iv [iv zvoid] #:ev [ev zvoid]) (zhash cap iv ev (make-hash) (make-vector cap iv) (list)))
 (define (zhash-keys arg-zhash) (zhash-k2i arg-zhash))
 (define (zhash-vals arg-zhash) (zhash-vvec arg-zhash))
+
+(define (decomposible? v)
+    (if (symbolic? v)
+        ; symbolic
+        (cond 
+            ; a union is decomposible
+            [(union? v) #t]
+
+            ; symbolic constant is not decomposible
+            [(constant? v) #f]
+
+            ; for expression, it could be `ite` or other forms (e.g., +/-)
+            ; we use reflecting provided publicly by rosette rather than hacking
+            [(expression? v)
+                (match v
+                    ; (fixme) there are more builtin rosette operators that you need to consider
+                    ;         e.g., bitvector->integer
+                    [(expression op child ...) (or
+                        (equal? 'ite* (object-name op))
+                        (equal? 'ite (object-name op))
+                    )]
+                    [_ (println-and-exit "# [exception] you can't reach here.")] ; for debugging in case anyone overrides `expression`
+                )
+            ]
+
+            ; (note) (important) this category usually corresponds to a collection that contains symbolic values
+            ;                    note that `symbolic?` is contagious, it mark a value as symbolic as long as any
+            ;                    of its member is symbolic
+            ; e.g., a struct instance with a symbolic member belongs to this category, and regarding decomposibility,
+            ; since it's for deciding whether one should use `for/all` (and whether new values will be revealed under `for/all`),
+            ; this category does not require decomposing because `for/all` reveals no new values
+            [else #f]
+        )
+        ; not symbolic, so not decomposible
+        #f
+    )
+)
 
 ; return a copy of a newly constructed list with element in the designated position replaced by given one
 ; - arg-ind should be concrete, if not concrete, the caller should wrap a for/all first
 ; - arg-val can be anything
 ; - arg-list can be symbolic, but by construction, we don't need for/all here
 (define (zhash-val-set! arg-zhash arg-ind arg-val)
-    (when (or (not (integer? arg-ind)) (symbolic? arg-ind)) (println-and-exit (format "# [zhash-panic] zhash-val-set: arg-ind should be a concrete integer, got: ~a." arg-ind)))
+    (when (or (not (integer? arg-ind)) (decomposible? arg-ind)) (println-and-exit (format "# [zhash-panic] zhash-val-set: arg-ind should be a concrete integer, got: ~a." arg-ind)))
     (let ([vvec (zhash-vvec arg-zhash)])
         (cond
             ; (note) vvec here in rosette can be
@@ -52,12 +89,14 @@
 ; check the key-index-map for existence of a key
 ; - arg-key should be concrete, if not concrete, the caller should wrap a for/all first
 (define (zhash-key-exists? arg-zhash arg-key)
-    (when (symbolic? arg-key) (println-and-exit (format "# [zhash-panic] zhash-key-exists?: arg-key should be concrete, got: ~a." arg-key)))
+    (when (decomposible? arg-key) (println-and-exit (format "# [zhash-panic] zhash-key-exists?:
+                                                                          arg-key should be concrete or symbolic term, got: ~a." arg-key)))
     (let ([k2i (zhash-k2i arg-zhash)])
         (hash-has-key? k2i arg-key)
     )
 )
 
+; TODO: change this for symbolic constants
 ; check the **factual** existence of a key
 ; if the corresponding value of this key is zvoid, it means the key is **factually** non-existent
 (define (zhash-has-key? arg-zhash arg-key)
@@ -86,7 +125,7 @@
 (define (zhash-secure-key! arg-zhash arg-key)
     ; (printf "# securing key: ~a\n" arg-key)
     (cond
-        [(symbolic? arg-key) (for/all ([dkey arg-key #:exhaustive]) (zhash-secure-key! arg-zhash dkey))]
+        [(decomposible? arg-key) (for/all ([dkey arg-key #:exhaustive]) (zhash-secure-key! arg-zhash dkey))]
         [else
             (when (not (zhash-key-exists? arg-zhash arg-key))
                 (let ([k2i (zhash-k2i arg-zhash)])
@@ -118,20 +157,32 @@
 (define (zhash-set! arg-zhash arg-key arg-val)
     ; first secure all the keys
     ; this will update val-list to make sure of sufficient slots before actual filling of values
+    (define prev-hash-keys (hash-keys (zhash-k2i arg-zhash)))
     (zhash-secure-key! arg-zhash arg-key)
     ; then set the value
+
     (define (exhaustive-set! ex-key)
         (cond
-            [(symbolic? ex-key) (for/all ([dkey ex-key #:exhaustive]) (exhaustive-set! dkey))]
+            [(decomposible? arg-key) (for/all ([dkey ex-key #:exhaustive]) (exhaustive-set! dkey))]
             [else
                 (let* ([k2i (zhash-k2i arg-zhash)]
                        [ind (hash-ref k2i ex-key)])
                     (zhash-val-set! arg-zhash ind arg-val)
+                    (when (term? ex-key)
+                          (define (conditional_update old-key)
+                                (when (= ex-key old-key)
+                                    (zhash-val-set! arg-zhash (hash-ref k2i old-key) arg-val)
+                                )
+                          )
+                          (for-each conditional_update prev-hash-keys)
+                    )
                 )
             ]
         )
     )
+
     (exhaustive-set! arg-key)
+
 )
 
 (define (zhash-clear! arg-zhash)
@@ -139,6 +190,6 @@
     (vector-fill! (zhash-vvec arg-zhash) zvoid))
 
 (define z (make-zhash 10))
-(define l (list 1 2 3 4))
-(define-symbolic b integer?)
-(define k (list-ref l b))
+;(define l (list 1 2 3 4))
+(define-symbolic b c integer?)
+;(define k (list-ref l b))
