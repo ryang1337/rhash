c79091047a12dd92737ad0ca398b6bb66509460a
Richard Yang <churd@Richards-MacBook-Pro.local>
2022-05-18 15:31:31 -0700

    changed implementation of termeq to be faster

--- subst.rkt
+++ subst.rkt
@@ -1,19 +1,26 @@
 #lang rosette
+(require rackunit)
+(require/expose rosette/base/core/term (term-val))
 
-(define (subst symterm sublist)
-    (define symconstlist (symbolics symterm))
-    
+(define (subst symterm substmapping)
     ; go through each of the consts in symconstlist, then evaluate symterm with
     ; the assertion that the first const in symconstlist is equal to
     ; the first value in sublist
 
     (cond
-        [(null? symconstlist) symterm]
+        [(not (term? symterm)) symterm]
         [else
-            (let* ([symconst (car symconstlist)]
-                   [sub (car sublist)]
-                   [symterm-subst (evaluate symterm (solve (assert (= symconst sub))))])
-                (subst symterm-subst (cdr sublist))
+            (if (constant? symterm)
+                (if (hash-has-key? substmapping symterm)
+                    (hash-ref substmapping symterm)
+                    symterm
+                )
+                (let* ([symval (term-val symterm)]
+                       [op (list-ref symval 0)]
+                       [val1 (list-ref symval 1)]
+                       [val2 (list-ref symval 2)])
+                     (op (subst val1 substmapping) (subst val2 substmapping))
+                )
             )
         ]
     )
